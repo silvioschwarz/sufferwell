7d729912fe78c312c82289972c3e2faba0eb9446
Silvio Schwarz <admin@silvioschwarz.xyz>
2026-08-15 14:35:52 +0200

    redesigned the sidebar

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const sidebar = document.querySelector('.sidebar');
-sidebar.querySelector('.blocker').onclick = hide;
-function show() { // swipe right
-  sidebar.classList.add('visible');
-  document.body.style.overflow = 'hidden';
+const sidebar = document.querySelector(".sidebar");
+const root = document.querySelector(":root");
+
+function show() {
+  // swipe right
+  sidebar.style.width = "15dvh";
+  root.style.setProperty("--sidenav-margin", "15dvh");
+  sidebar.classList.add("visible");
+  document.body.style.overflow = "hidden";
 }
-function hide() { // by blocker click, swipe left, or url change
-  sidebar.classList.remove('visible');
-  document.body.style.overflow = '';
+function hide() {
+  // by blocker click, swipe left, or url change
+  sidebar.style.width = "0px";
+  root.style.setProperty("--sidenav-margin", "0px");
+
+  sidebar.classList.remove("visible");
+  document.body.style.overflow = "";
 }
 function toggle() {
-  sidebar.classList.contains('visible') ? hide() : show();
+  sidebar.classList.contains("visible") ? hide() : show();
 }
+
